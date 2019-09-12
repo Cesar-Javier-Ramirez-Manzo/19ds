30af3dd2fc0b6b14da28675c184d4cbf40de0629
Cesar-Javier-Ramirez-Manzo <50338995+Cesar-Javier-Ramirez-Manzo@users.noreply.github.com>
2019-09-12 10:31:31 -0500

    Update sumatoria 4.js

--- oracles/sumatoria 4.js
+++ oracles/sumatoria 4.js
@@ -1,12 +1,14 @@
-var x=0
+var x=4
+let k=0
 
 for(var i=1;i<=10;i++){
-    var x=x+(Math.pow(i,7))
+    k=k+(Math.pow(i,x))//acumulador de la suma de las potencias 
 
    
 }
 
-console.log('El resultado de la sumatoria es '+x)
+console.log('El resultado de la sumatoria es '+k)
 
-var d=1/(7+1)*(Math.pow(10,8))
+var d=(Math.pow(10,(x+1)))/(x+1)//formula, los resultaods son parecidos aunque no iguales
+console.log(typeof d)
 console.log('el resultado de la formula es '+d) 
